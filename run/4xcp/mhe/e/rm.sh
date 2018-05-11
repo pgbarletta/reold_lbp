da0c11d5db7c18e417e5da881234d3507eb7c989
pbarletta <pbarletta@gmail.com>
2018-05-11 20:10:05 -0300

    Anlyzed 4XCP cavity and got PCA modes

---
+++ run/4xcp/mhe/e/rm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ls ??equ.in | grep -v 30 | xargs rm
+ls ?equ.in | grep -v 0 | xargs rm
+rm mdinfo nohup.out *rst7 *nc *out
---
+++ run/4xcp/ndd/disp_/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./ndd.jl -p ../avg_4xcp.pdb -v ../../pca/full_modes_4xcp -o 4xcp -a true -m 10
+
+exit 0
