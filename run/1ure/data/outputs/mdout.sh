69ea36489855276fd6431253229f7f9149b053bb
pbarletta <pbarletta@gmail.com>
2018-05-12 17:08:45 -0300

    Checked 1URE run and got PCA

---
+++ run/1ure/data/outputs/mdout.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Extract heat and equil mdout info
+perl process_mdout.perl 1pdt.out 2pdt.out 3pdt.out 4pdt.out 5pdt.out 6pdt.out 7pdt.out 8pdt.out 9pdt.out 10pdt.out
+
+mv summary.ETOT etot.dat
+mv summary.EPTOT eptot.dat
+mv summary.EKTOT ektot.dat
+mv summary.TEMP temp.dat
+mv summary.PRES pres.dat
+mv summary.VOLUME vol.dat
+mv summary.DENSITY density.dat
+
+rm summary*
+
