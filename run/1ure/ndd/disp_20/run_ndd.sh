1583bdfcffd8674bff22e12b160a3caea260927d
pbarletta <pbarletta@gmail.com>
2018-05-13 11:31:18 -0300

    Done volume along 1URE trajectory, NDD, and VGV

---
+++ run/1ure/ndd/disp_20/run_ndd.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./ndd.jl -p ../avg_1ure.pdb -v ../../pca/full_modes_1ure -o 1ure -a true -m 5
+
+exit 0
---
+++ run/1ure/ndd/run_vgv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ANA avg_1ure.pdb -c beta.cfg --NDD_input  in_ndd_avg_1ure --NDD_output  out_ndd_1ure
+
+exit 0
