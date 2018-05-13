3a08d42a43f72d233e12ccf8ac482ec1fbd41a94
pbarletta <pbarletta@gmail.com>
2018-05-13 11:31:42 -0300

    Redone 4UET, NDD, and done VGV

--- run/4uet/ndd/disp_20/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./ndd.jl -p ../avg_4uet.pdb -v ../../pca/full_modes_4uet -o 4uet -a true -m 10
-
-exit 0
---
+++ run/4uet/ndd/run_vgv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ANA avg_4uet.pdb -c alfa.cfg --NDD_input  in_ndd_avg_4uet --NDD_output  out_ndd_4uet
+
+exit 0
