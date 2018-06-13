0d95d3c2acc6a962297856308cd05dd8fb62630d
pbarletta <pbarletta@gmail.com>
2018-06-13 13:52:05 -0300

    Parametrized volumes distribution from projections

--- run/4xcp/cavidad/run.sh
+++ run/4xcp/cavidad/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-nohup ANA full_avg_4xcp.pdb -d ../data/full_fit_4xcp.nc -v vol_trj_4xcp &
+nohup ANA full_avg_4xcp.pdb -c alfa.cfg -d ../data/full_fit_4xcp.nc -v vol_trj_4xcp &
 
 exit 0
