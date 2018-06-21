f67ed60f69d13415fb42ec917ca25f59f4858098
pbarletta <pbarletta@gmail.com>
2018-06-21 20:03:19 -0300

    Updated 4xcp analysis. Dropped beta proteins.

--- run/4uet/data/outputs/mdout.sh
+++ run/4uet/data/outputs/mdout.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Extract heat and equil mdout info
-perl process_mdout.perl 1pdt.out  2pdt.out  3pdt.out 4pdt.out
+perl process_mdout.perl 1pdt.out 2pdt.out 3pdt.out 4pdt.out 5pdt.out 6pdt.out 7pdt.out 8pdt.out 9pdt.out 10pdt.out 11pdt.out 12pdt.out 13pdt.out 14pdt.out 15pdt.out 16pdt.out 17pdt.out 18pdt.out 19pdt.out 20pdt.out 21pdt.out 22pdt.out 23pdt.out 24pdt.out
+
 
 mv summary.ETOT etot.dat
 mv summary.EPTOT eptot.dat
