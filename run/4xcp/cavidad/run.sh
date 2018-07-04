1730a99efd7db5cc150debe7d51e459c5446a75f
pbarletta <pbarletta@gmail.com>
2018-07-04 16:14:04 -0300

    Began analyzing 4XCP final run. Protein side of things done.

--- run/4xcp/cavidad/run.sh
+++ run/4xcp/cavidad/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-nohup ANA full_avg_4xcp.pdb -c alfa.cfg -d ../data/full_fit_4xcp.nc -v vol_trj_4xcp &
+#ANA full_avg_4xcp.pdb -c alfa.cfg -d ../data/full_fit_4xcp.nc -v vol_trj_4xcp
+ANA full_avg_4xcp.pdb -c 100_alfa.cfg -d ../data/full_fit_4xcp.nc -o cav_trj_4xcp
 
 exit 0
--- run/4xcp/ndd/run_static.sh
+++ run/4xcp/ndd/run_static.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ANA avg_4xcp.pdb -c alfa.cfg -o cav_avg_4xcp > vol_avg_4xcp
+ANA avg_4xcp.pdb -c alfa_cut.cfg -o cav_avg_4xcp > vol_avg_4xcp
 
 exit 0
