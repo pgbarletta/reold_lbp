2088a3841d45fcd2fd0969d13b41f5f035286e84
pbarletta <pbarletta@gmail.com>
2018-07-04 19:47:03 -0300

    plotted harmonic energies for each conformer

--- run/4xcp/poblaciones/ndd/0/run_static.sh
+++ run/4xcp/poblaciones/ndd/0/run_static.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ANA avg_4xcp_0.pdb -c alfa.cfg -o cav_avg_4xcp_0 > vol_avg_4xcp_0
+ANA avg_4xcp_0.pdb -c alfa_cut.cfg -o cav_avg_4xcp_0 > vol_avg_4xcp_0
 
 exit 0
--- run/4xcp/poblaciones/ndd/2/run_static.sh
+++ run/4xcp/poblaciones/ndd/2/run_static.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ANA avg_4xcp_2.pdb -c alfa.cfg -o cav_avg_4xcp_2 > vol_avg_4xcp_2
+ANA avg_4xcp_2.pdb -c alfa_cut.cfg -o cav_avg_4xcp_2 > vol_avg_4xcp_2
 
 exit 0
