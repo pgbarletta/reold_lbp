2b863e689c57dabaa8396b1786eee111ace71669
pbarletta <pbarletta@gmail.com>
2018-07-24 14:58:36 -0300

    Redone plots with Plots, gr(). Analyzing 1URE

--- run/1ure/cavidad/run.sh
+++ run/1ure/cavidad/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-nohup ANA avg_1ure.pdb -c beta.cfg -d ../data/full_fit_1ure.nc -v vol_trj_1ure &
+nohup ANA avg_1ure.pdb -c cut_beta.cfg -d ../data/full_fit_1ure.nc -v vol_trj_1ure &
+#nohup ANA 99999_1ure.pdb -c 100_cut_beta.cfg -d ../data/full_fit_1ure.nc -o cav_trj_1ure &
 
 exit 0
--- run/1ure/ndd/run_static.sh
+++ run/1ure/ndd/run_static.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ANA avg_1ure.pdb -c beta.cfg -o cav_avg_1ure > vol_avg_1ure
+ANA avg_1ure.pdb -c cut_beta.cfg -v vol_avg_1ure
 
 exit 0
