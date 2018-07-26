fe58de2c980cf2d2a08388908dc8bde63c5afa3f
pbarletta <pbarletta@gmail.com>
2018-07-26 20:06:49 -0300

    ñam

--- run/4uet/cavidad/run.sh
+++ run/4uet/cavidad/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-nohup ANA full_avg_4uet.pdb -c alfa.cfg -d ../data/full_fit_4uet.nc -v vol_trj_4uet &
-#nohup ANA full_avg_4uet.pdb -c alfa.cfg -d ../data/full_fit_4uet.nc -o trj_cav_4uet &
+ANA avg_cut_4uet.pdb -c alfa_cut.cfg -d ../data/full_fit_4uet.nc -v vol_trj_4uet
+#ANA avg_cut_4uet.pdb -c alfa.cfg -d ../data/full_fit_4uet.nc -o trj_cav_4uet
 
 exit 0
--- run/4xcp/cavidad/run.sh
+++ run/4xcp/cavidad/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#ANA full_avg_4xcp.pdb -c alfa.cfg -d ../data/full_fit_4xcp.nc -v vol_trj_4xcp
-ANA full_avg_4xcp.pdb -c 100_alfa.cfg -d ../data/full_fit_4xcp.nc -o cav_trj_4xcp
+ANA cut_99999_4xcp.pdb -c alfa_cut.cfg -d ../data/full_fit_4xcp.nc -v vol_trj_4xcp
+#ANA full_avg_4xcp.pdb -c 100_alfa.cfg -d ../data/full_fit_4xcp.nc -o cav_trj_4xcp
 
 exit 0
