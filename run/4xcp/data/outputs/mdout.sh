aed523a3ddc9fd4100e5a6a7b3d3c17ebcb6f4d9
pbarletta <pbarletta@gmail.com>
2018-08-07 16:35:53 -0300

    Got extra us, for 4XCP, 5us total. Just in case. Checked on alfa_mhe&run.ipynb

--- run/4xcp/data/outputs/mdout.sh
+++ run/4xcp/data/outputs/mdout.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Extract heat and equil mdout info
-perl process_mdout.perl 1pdt.out 2pdt.out 3pdt.out 4pdt.out 5pdt.out 6pdt.out 7pdt.out 8pdt.out 9pdt.out 10pdt.out 11pdt.out 12pdt.out 13pdt.out 14pdt.out 15pdt.out 16pdt.out 17pdt.out 18pdt.out 19pdt.out 20pdt.out 21pdt.out 22pdt.out 23pdt.out 24pdt.out 25pdt.out 26pdt.out 27pdt.out 28pdt.out 29pdt.out 30pdt.out 31pdt.out 32pdt.out 33pdt.out 34pdt.out
+perl process_mdout.perl 1pdt.out 2pdt.out 3pdt.out 4pdt.out 5pdt.out 6pdt.out 7pdt.out 8pdt.out 9pdt.out 10pdt.out 11pdt.out 12pdt.out 13pdt.out 14pdt.out 15pdt.out 16pdt.out 17pdt.out 18pdt.out 19pdt.out 20pdt.out 21pdt.out 22pdt.out 23pdt.out 24pdt.out 25pdt.out 26pdt.out 27pdt.out 28pdt.out 29pdt.out 30pdt.out 31pdt.out 32pdt.out 33pdt.out 34pdt.out 35pdt.out 36pdt.out 37pdt.out 38pdt.out 39pdt.out 40pdt.out 41pdt.out 42pdt.out 43pdt.out 44pdt.out nohup.out strip_cpp_out
 
 mv summary.ETOT etot.dat
 mv summary.EPTOT eptot.dat
