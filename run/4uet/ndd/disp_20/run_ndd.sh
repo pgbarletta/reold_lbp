3e9b7b82da7eb1393aac534e52fcbdee476714ad
German <pbarletta@gmail.com>
2018-10-10 22:30:16 -0300

    warf, warf!

--- run/4uet/ndd/disp_20/run_ndd.sh
+++ run/4uet/ndd/disp_20/run_ndd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./ndd.jl -p ../avg_4uet.pdb -v ../../pca/full_modes_4uet -o 4uet -a true -m 5
+~/labo/ANA/scripts/ndd.jl -p ../avg_4uet.pdb -v ../../pca/full_modes_4uet -o 4uet -a true -m 5
 
 exit 0
