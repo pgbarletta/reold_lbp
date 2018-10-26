051a1fcfd06138c424965f9fbc3a1c1f534cf9e5
German <pbarletta@gmail.com>
2018-10-26 16:33:25 -0300

    Got 3D PCA projections.

--- run/1ure/ndd/disp_20/run_ndd.sh
+++ run/1ure/ndd/disp_20/run_ndd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./ndd.jl -p ../avg_1ure.pdb -v ../../pca/full_modes_1ure -o 1ure -a true -m 5
+/usr/local/julia-0.7.0/bin/julia ~/labo/ANA/scripts/ndd.jl -p ../avg_1ure.pdb -v ../../pca/full_modes_1ure -o 1ure -a true -m 5
 
 exit 0
