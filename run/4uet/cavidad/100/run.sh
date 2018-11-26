980e18f7bb24b618c2587cad61f14377c98db74d
German <pbarletta@gmail.com>
2018-11-26 16:14:45 -0300

    Checked cavity lining residues

---
+++ run/4uet/cavidad/100/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ANA avg_4uet.pdb -c mod_alfa_cut.cfg -d ../../data/full_fit_4uet.nc -v vol_avg_4uet
+
+exit 0
---
+++ run/4xcp/cavidad/100/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ANA avg_4xcp.pdb -c mod_alfa_cut.cfg -d ../../data/full_fit_4xcp.nc -v vol_avg_4xcp
+
+exit 0
